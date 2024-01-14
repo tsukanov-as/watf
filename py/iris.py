bc0518c61d69cf05067ce74d7f9e40b3e8bb3c9e
tsukanov-as <tsukanov.as@yandex.ru>
2024-01-14 19:29:57 +0300

    add iris

---
+++ py/iris.py
@@ -0,0 +1,26 @@
+from sklearn import datasets
+
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+
+input_count = 4
+output_count = 3
+
+import watf
+
+c = watf.Сlassifier(output_count, input_count)
+
+# train
+for epoch in range(50):
+    total_misses = c.tune_all(y_train, x_train)
+
+    train_accuracy = c.test_all(y_train, x_train)
+    test_accuracy = c.test_all(y_test, x_test)
+    print("[epoch %d] accuracy train: %f; accuracy test: %f" % (epoch, train_accuracy, test_accuracy))
+
+    if total_misses == 0:
+        break
