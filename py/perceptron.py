9b2baea735d33fe819da28bff91627c91189dcd1
tsukanov-as <tsukanov.as@yandex.ru>
2024-01-27 23:56:07 +0300

    fix perceptron

--- py/perceptron.py
+++ py/perceptron.py
@@ -1,26 +1,27 @@
 import numpy as np
 
 class Сlassifier:
-    def __init__(self, L, F):
-        self.W = np.zeros((L, F))
-        self.B = np.zeros(L)
+    def __init__(self, Y, X):
+        self.W = np.zeros((Y, X))
+        self.B = np.zeros(Y)
 
-    def tune(self, L, F):
-        delta = L - np.heaviside(self.W @ F + self.B, 0)
-        self.W += np.einsum('i,j->ij', delta, F)
+    def tune(self, Y, X):
+        P = np.heaviside(self.W @ X + self.B, 0)
+        delta = Y - P
+        self.W += np.einsum('i,j->ij', delta, X)
         self.B += delta
 
-    def tune_all(self, L, F):
-        L = np.eye(self.W.shape[0])[L]
-        for i in range(len(F)):
-            self.tune(L[i], F[i])
+    def tune_all(self, Y, X):
+        Y = np.eye(self.W.shape[0])[Y]
+        for i in range(len(X)):
+            self.tune(Y[i], X[i])
 
-    def pred(self, F):
-        return (self.W @ F + self.B).argmax()
+    def pred(self, X):
+        return np.heaviside(self.W @ X + self.B, 0).argmax()
 
-    def test_all(self, L, F):
+    def test_all(self, Y, X):
         total = 0
-        for i in range(len(F)):
-            if self.pred(F[i]) == L[i]:
+        for i in range(len(X)):
+            if self.pred(X[i]) == Y[i]:
                 total += 1
-        return total/len(F)
+        return total/len(X)
--- py/perceptron_sigmoid.py
+++ py/perceptron_sigmoid.py
@@ -4,30 +4,32 @@ def sigmoid(v):
     return 1 / (1 + np.exp(-v))
 
 def sigmoid_deriv(v):
-    return v * (1-v)
+    return v * (1 - v)
+
+LR = 0.01
 
 class Сlassifier:
-    def __init__(self, L, F):
-        self.W = np.zeros((L, F))
-        self.B = np.zeros(L)
+    def __init__(self, Y, X):
+        self.W = np.zeros((Y, X))
+        self.B = np.zeros(Y)
 
-    def tune(self, L, F):
-        Y = sigmoid(self.W @ F + self.B)
-        delta = (L-Y) * sigmoid_deriv(Y)
-        self.W += np.einsum('i,j->ij', delta, F)
+    def tune(self, Y, X):
+        P = sigmoid(self.W @ X + self.B)
+        delta = (Y - P) * sigmoid_deriv(P) * LR
+        self.W += np.einsum('i,j->ij', delta, X)
         self.B += delta
 
-    def tune_all(self, L, F):
-        L = np.eye(self.W.shape[0])[L]
-        for i in range(len(F)):
-            self.tune(L[i], F[i])
+    def tune_all(self, Y, X):
+        Y = np.eye(self.W.shape[0])[Y]
+        for i in range(len(X)):
+            self.tune(Y[i], X[i])
 
-    def pred(self, F):
-        return (self.W @ F + self.B).argmax()
+    def pred(self, X):
+        return sigmoid(self.W @ X + self.B).argmax()
 
-    def test_all(self, L, F):
+    def test_all(self, Y, X):
         total = 0
-        for i in range(len(F)):
-            if self.pred(F[i]) == L[i]:
+        for i in range(len(X)):
+            if self.pred(X[i]) == Y[i]:
                 total += 1
-        return total/len(F)
+        return total/len(X)
